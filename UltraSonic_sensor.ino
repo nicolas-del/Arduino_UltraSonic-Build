bad905308f7fb7d8c3d001ea201bdf6fe2f7542a
Nicolas Espitalier <nespitalier013@gmail.com>
2020-10-18 20:58:07 -0400

    Initial Commit

---
+++ UltraSonic_sensor.ino
@@ -0,0 +1,67 @@
+const int echo = 13;
+const int trig = 12;
+
+const int blue_led = 2;
+const int green_led = 3;
+const int yellow_led = 4;
+const int red_led = 5;
+
+int duration = 0;
+int distance = 0;
+
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  pinMode(blue_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(yellow_led, OUTPUT);
+  pinMode(red_led, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trig, HIGH);
+  delay(10);
+  digitalWrite(trig, LOW);
+
+  duration = pulseIn(echo, HIGH);
+  distance = (duration/2) / 28.5;
+  Serial.println(distance);
+
+  if(distance >= 0 && distance <= 15) //Activate the red led between these distance, else it won't activate
+  {
+    digitalWrite(red_led, HIGH);
+  }
+  else
+  {
+    digitalWrite(red_led, LOW);  
+  }
+  
+  if(distance >= 16 && distance <= 30) 
+  {
+    digitalWrite(yellow_led, HIGH);  
+  }
+  else
+  {
+    digitalWrite(yellow_led, LOW);
+  }
+
+  if(distance >= 31 && distance <= 45)
+  {
+    digitalWrite(green_led, HIGH);  
+  }
+  else
+  {
+    digitalWrite(green_led, LOW);
+  }
+
+  if(distance >= 46 && distance <= 100)
+  {
+    digitalWrite(blue_led, HIGH);  
+  }
+  else
+  {
+    digitalWrite(blue_led, LOW);
+  }
+}
